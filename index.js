05e1d70bb77886dc289eaa154e9f1addef86f8d8
Kevin Grandon <kevingrandon@yahoo.com>
2013-12-06 10:53:31 -0500

    Basic digital write

--- index.js
+++ index.js
@@ -1,9 +1,32 @@
-function Sparky() {
+var exec = require('child_process').exec;
+var child;
 
+function Sparky(config) {
+
+	if (!config.deviceId) {
+		console.log('Device id not found.')
+	}
+
+	if (!config.token) {
+		console.log('Spark token not found.')
+	}
+
+	this.config = config
 }
 
 Sparky.prototype = {
+	digitalWrite: function(pin, value) {
+		var command = 'curl https://api.spark.io/v1/devices/' + this.config.deviceId + '/digitalwrite   -d access_token=' + this.config.token + ' -d params=' + pin + ',' + value;
 
+		child = exec(command,
+			function (error, stdout, stderr) {
+			//console.log('stdout: ' + stdout);
+			//console.log('stderr: ' + stderr);
+			if (error !== null) {
+			  console.log('exec error: ' + error);
+			}
+		});
+	}
 }
 
 exports.Sparky = Sparky
