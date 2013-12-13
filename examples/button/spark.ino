57c4a58618ecabe1187a8adef45fe25b25492a41
Kevin Grandon <kevingrandon@yahoo.com>
2013-12-12 16:22:08 -0800

    Whitespace

--- examples/button/spark.ino
+++ examples/button/spark.ino
@@ -2,11 +2,11 @@ int buttonPin = 5; 			 // pin of button
 int buttonState = 0;         // variable for reading the pushbutton status
 
 void setup() {
-    Spark.function("laststate", laststate);
+	Spark.function("laststate", laststate);
 }
 
 void loop() {
- 	pinMode(buttonPin, INPUT_PULLDOWN);
+	pinMode(buttonPin, INPUT_PULLDOWN);
 	buttonState = digitalRead(buttonPin);
 	delay(250);
 }
