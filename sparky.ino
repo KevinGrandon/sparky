739b0043018c1904a37b55fdf8cc1558392f293b
Kevin Grandon <kevingrandon@yahoo.com>
2013-12-13 21:37:51 -0800

    Update generic sketch

--- sparky.ino
+++ sparky.ino
@@ -3,47 +3,6 @@
 // set of devices and components. Hopefully we can contribute similar
 // changes in the future to the core tinker app.
 
-// Array of components
-// Each "component" sits on a pin
-Component[14] *components;
-
-void setup() {
-	Spark.function("attach", attach);
-	Spark.function("read", read);
-	Spark.function("write", write);
-}
-
-void loop() {
-
-}
-
-int attach(String command)
-{
-	String type = strtok(command, ",");
-	int pin = atoi(strtok(NULL, ","));
-
-	if (type === "servo") {
-		components[pin] = new SparkyServo(pin, _command);
-		return 1;
-	} else if (type === "button") {
-		components[pin] = new SparkyButton(pin, _command);
-		return 1;
-	}
-	return -1;
-}
-
-int write(String command)
-{
-	int pin = atoi(strtok(command, ","));
-	components[pin] -> write(command);
-	return 1;
-}
-
-int read(String command)
-{
-	int pin = atoi(strtok(command, ","));
-	components[pin] -> read(command);
-}
 
 class Component
 {
@@ -55,11 +14,12 @@ class Component
 		_pin = pin;
 	}
 
-	virtual void write()
+	int write(String command)
 	{
+		return 1;
   	}
 
-  	virtual int read()
+  	int read(String command)
 	{
 		return digitalRead(_pin);
 	}
@@ -78,7 +38,7 @@ class SparkyButton : public Component
 		pinMode(pin, INPUT_PULLDOWN);
 	}
     
-	virtual int read()
+	int read()
 	{
 		return digitalRead(_pin);
 	}
@@ -91,17 +51,64 @@ class SparkyServo : public Component
 {
   public:
     SparkyServo();
-	SparkyServo(String command)
+	SparkyServo(int pin, String command)
 	{
-		_pin = atoi(strtok(NULL, ","));
+		_pin = pin;
 		_servo.attach(_pin);
 	}
 
-	virtual void write(int val)
+	int write(int val)
 	{
 		_servo.write(val);
+		return 1;
   	}
   private:
     Servo _servo;
     int _pin;
 };
+
+// Array of components
+// Each "component" sits on a pin
+Component components[14];
+
+void setup() {
+	Spark.function("attach", attach);
+	Spark.function("read", read);
+	Spark.function("write", write);
+}
+
+void loop() {
+
+}
+
+int attach(String command)
+{
+	char* buf = strdup(command);
+	String type = strtok(buf, ",");
+	int pin = atoi(strtok(NULL, ","));
+
+	if (type === "servo") {
+		components[pin] = new SparkyServo(pin, command);
+		return 1;
+	} else if (type === "button") {
+		components[pin] = new SparkyButton(pin, command);
+		return 1;
+	}
+	return -1;
+}
+
+int write(String command)
+{
+	char* buf = strdup(command);
+	int pin = atoi(strtok(buf, ","));
+	components[pin].write(command);
+	return 1;
+}
+
+int read(String command)
+{
+	char* buf = strdup(command);
+	int pin = atoi(strtok(buf, ","));
+	components[pin].read(command);
+	return 1;
+}
